99b9de0bb09a23e691e9df56943dd7aba7fcd58f
rod608 <rod608ney@gmail.com>
2022-08-28 11:51:35 -0400

    Changed the OrganizeDesktop class's preset paths

--- app/organize/org_desktop.py
+++ app/organize/org_desktop.py
@@ -4,9 +4,9 @@ from definitions import Definitions
 
 
 class OrganizeDesktop(OrganizeDirectory):
-    """ Class for Organizing files on the Desktop. Files should be moved into folders. """
+    """ Class for Organizing files on the Desktop to the Documents Folder. Files should be moved into folders. """
 
     def __init__(self, formats_obj: Format, folders: dict[str, str]):
         """ Constructor. Same as OrganizeDirectory's, but with preset paths. """
-        desktop_path: tuple[str, str] = (Definitions.DESKTOP_PATH, Definitions.DESKTOP_PATH)
-        super().__init__(formats_obj, desktop_path, folders)
+        old_to_new_path: tuple[str, str] = (Definitions.DESKTOP_PATH, Definitions.DOCUMENTS_PATH)
+        super().__init__(formats_obj, old_to_new_path, folders)

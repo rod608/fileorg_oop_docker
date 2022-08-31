8cb82f06d38b2142f1b128786096f2c03fc12c9a
rod608 <rod608ney@gmail.com>
2022-08-31 12:22:53 -0400

    Logging now works. time for testing.

--- testing.py
+++ testing.py
@@ -1,7 +1,20 @@
 """ running simple checks! """
+import logging.config
+import os
 
+import logging_config
+from definitions import Definitions
 
 # main method; signifies a script
 if __name__ == "__main__":
-    pass
-    # time for logging.
+    # set up the logs.
+
+    # ensure the log directory exists.
+    if not os.path.exists(Definitions.LOG_DIR):
+        os.mkdir(Definitions.LOG_DIR)
+        print("Directory now exists.")
+
+    # configure logging setup.
+    logging.config.dictConfig(logging_config.LOGGING_CONFIG_DICT)
+    logger = logging.getLogger("org_logger")
+    logger.info("Hello.")

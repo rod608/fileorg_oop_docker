b2cdd4da9789dbe40fbf8cfa20fcf3abb11cce80
rod608 <rod608ney@gmail.com>
2022-09-01 18:35:51 -0400

    This will be where my main method resides

--- testing.py
+++ testing.py
@@ -7,12 +7,6 @@ from definitions import Definitions
 
 # main method; signifies a script
 if __name__ == "__main__":
-    # ensure the log directory exists.
-    if not os.path.exists(Definitions.LOG_DIR):
-        os.mkdir(Definitions.LOG_DIR)
-        print("Directory now exists.")
-
     # configure logging setup.
     logging.config.dictConfig(logging_config.LOGGING_CONFIG_DICT)
     logger = logging.getLogger("org_logger")
-    logger.warning("Test 2.")

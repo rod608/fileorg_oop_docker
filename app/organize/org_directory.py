73f97c6edae859b91c6b490026937e1be29e3df1
rod608 <rod608ney@gmail.com>
2022-09-02 14:50:48 -0400

    Added logging for all methods! Also added a helper method to _move_files() that helps with code repitition.

--- app/organize/org_directory.py
+++ app/organize/org_directory.py
@@ -1,8 +1,11 @@
 import os
 import shutil
+import logging
+import logging.config
 
 from pathlib import Path
 from app.organize.formats import Format
+from logging_config import LOGGING_CONFIG_DICT
 
 
 class OrganizeDirectory:
@@ -14,6 +17,9 @@ class OrganizeDirectory:
         self._og_path = paths[0]     # Directory w/ Files to Organize.
         self._final_path = paths[1]  # Target Directory. Folders are created, files are moved.
         self._folders = folders if folders else {}  # format_type -> folder_name
+
+        # Logging config!
+        logging.config.dictConfig(LOGGING_CONFIG_DICT)
 
     def organize(self) -> None:
         """ Organize Method: Move all files w/ certain extensions into their specified folders. """
@@ -35,10 +41,14 @@ class OrganizeDirectory:
 
     def _files(self) -> list[Path]:
         """ Return a list of Path objects that are instantiated w/ the files in the original directory in mind. """
+        logger = logging.getLogger("org_logger")
+        logger.info(f"self._files(): Searching the og_location \"{self._og_path}\" for files...")
+
         os.chdir(self._og_path)
 
         files = []
         for item in os.listdir():
+            logger.info(f"cur_item = {item}")
             if item[0] == ".":
                 continue
 
@@ -46,21 +56,32 @@ class OrganizeDirectory:
             if not file.suffix:
                 continue
             else:
+                logger.info(f"Adding {file.name} to file_list.")
                 files.append(Path(item))
 
+        logger.info(f"file_list == {files}")
         return files
 
     def _create_folders(self) -> None:
         """ For each key in the 'folders' dictionary, create a folder in the final dir if the format type is valid.  """
+        logger = logging.getLogger("org_logger")
+        logger.info(f"self._create_folders(): Creating folders in the final_path \"{self._final_path}\"")
+
         os.chdir(self._final_path)
 
         if self._folders:
             for format_type in self._formats.formats:
                 if format_type in self._folders:
+                    logger.info(f"Creating folder: {self._folders[format_type]}")
                     Path(f"{self._folders[format_type]}").mkdir(exist_ok=True)
+                    logger.info(f"folder created or already exists.")
+
 
     def _rm_empty_folders(self) -> None:
         """ After organization, delete all empty folders. """
+        logger = logging.getLogger("org_logger")
+        logger.info(f"self._rm_empty_folders(): Removing empty folders in the final_path \"{self._final_path}\"")
+
         os.chdir(self._final_path)
 
         for item in os.listdir():
@@ -68,47 +89,61 @@ class OrganizeDirectory:
             folder = Path(item)
 
             if folder.is_dir():
+                logger.info(f"cur_folder = {folder.name}")
                 os.chdir(folder)
+
                 if not os.listdir():
+                    logger.info(f"Preparing to remove empty folder {folder.name}")
                     os.chdir(self._final_path)
                     folder.rmdir()
+                    logger.info(f"{folder.name} folder removed.")
 
+        logger.info(f"removal(s) complete. changing CWD to \"{self._final_path}\"")
         os.chdir(self._final_path)
 
     def _move_files(self, files: list[Path]) -> None:
         """ Move files from og_path to final_path, potentially within folders as well. """
+        logger = logging.getLogger("org_logger")
+        logger.info(f"self._move_files(): Preparing to move files from the \"{self._og_path}\"")
+
         os.chdir(self._og_path)
 
         for file in files:
             for file_type in self._formats.formats:
                 # Moving Process.
                 if file.suffix in self._formats.formats[file_type]:
+                    logger.info(f"Preparing to move file {file.name}")
                     final_location = self._final_path
 
                     # No folders specified.
                     if not self._folders:
                         # Move if the file doesn't already exist in the final path.
-                        if not os.path.exists(final_location + f"/{file.name}"):
-                            shutil.move(file, final_location)
-                        else:
-                            os.remove(file)
+                        self.__move_helper(final_location, file)
                         break
 
                     # File Ext tied to a folder. Move file to folders.
                     elif file_type in self._folders:
-                        final_location = final_location.rstrip("/") + f"/{self._folders[file_type]}"
+                        final_location = os.path.join(final_location, self._folders[file_type])
                         # Move if the file doesn't already exist in the final path.
-                        if not os.path.exists(final_location + f"/{file.name}"):
-                            shutil.move(file, final_location)
-                        else:
-                            os.remove(file)
+                        self.__move_helper(final_location, file)
                         break
 
                     # File Ext not tied to a folder. Move file to final destination.
                     else:
                         # Move if the file doesn't already exist in the final path.
-                        if not os.path.exists(final_location + f"/{file.name}"):
-                            shutil.move(file, final_location)
-                        else:
-                            os.remove(file)
+                        self.__move_helper(final_location, file)
                         break
+
+    @staticmethod
+    def __move_helper(final_location, file) -> None:
+        """ Helper function for move_files(). Given a file, move if only one of it exists otherwise delete it. """
+        logger = logging.getLogger("org_logger")
+
+        if not os.path.exists(os.path.join(final_location, file.name)):
+            logger.info(f"Moving to \"{final_location}\"")
+            shutil.move(file, final_location)
+            logger.info(f"File moved.")
+        else:
+            logger.info(f"Duplicate File {file.name} found. Removing newest entry.")
+            os.remove(file)
+            logger.info(f"File removed.")

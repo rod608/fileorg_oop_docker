65012ca68b69fa35b5453bd387bad24c6c31ba5f
rod608 <rod608ney@gmail.com>
2022-09-04 15:37:25 -0400

    Added automatic creation of the Desktop & Documents path in case they don't already exist.

--- tests/test_organize.py
+++ tests/test_organize.py
@@ -78,6 +78,11 @@ def test_move_files(org_dir) -> None:
     org_dir._formats.formats["fake_format"].add(".mp5")
     assert ".mp5" in org_dir._formats.formats["fake_format"]
 
+    # Create the Desktop & Documents directories if they don't exist. This will be the og_path.
+    if not os.path.exists(Definitions.DESKTOP_PATH) or not os.path.exists(Definitions.DOCUMENTS_PATH):
+        Path(Definitions.DESKTOP_PATH).mkdir(exist_ok=True)
+        Path(Definitions.DOCUMENTS_PATH).mkdir(exist_ok=True)
+
     # change CWD to the desktop, create fake mp5 video file.
     os.chdir(org_dir._og_path)
     assert os.getcwd() == Definitions.DESKTOP_PATH

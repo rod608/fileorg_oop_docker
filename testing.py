f1fe20dfa3b73bc5fdd8f5bab221fc50327cd523
rod608 <rod608ney@gmail.com>
2022-09-04 11:54:47 -0400

    Removed a lot of functionality as this was mainly for testing.

--- testing.py
+++ testing.py
@@ -1,12 +1,11 @@
 """ running simple checks! """
 import logging.config
-import os
 
 import logging_config
-from definitions import Definitions
 
 # main method; signifies a script
 if __name__ == "__main__":
     # configure logging setup.
-    logging.config.dictConfig(logging_config.LOGGING_CONFIG_DICT)
-    logger = logging.getLogger("org_logger")
+    print("Hi!")
+    # logging.config.dictConfig(logging_config.LOGGING_CONFIG_DICT)
+    # logger = logging.getLogger("org_logger")
